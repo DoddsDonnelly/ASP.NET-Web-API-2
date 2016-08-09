6c82db6ce182c0783f5a857e8c672dfafd0b63dd
DoddsDonnelly <crystaldodds@msn.com>
2016-08-09 07:55:02 -0700

    Environment Setup
    All chapter 4 exercises complete

---
+++ src/WebApi2Book.Web.Api.Models/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi2Book.Web.Api.Models
+{
+    internal class Task
+    {
+        private List<Link> _links;
+
+        public long? TaskId { get; set; }
+        public string Subject { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public Status Status { get; set; }
+        public List<User> Assigness { get; set; }
+
+        public List<Link> Links
+        {
+            get { return _links ?? (_links = new List<Link>()); }
+            set { _links = value; }
+        }
+
+        public void AddLink(Link link)
+        {
+            Links.Add(link);
+        }
+    }
+}
